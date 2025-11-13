6eea210d21ed60ba93c50a880e1db040b21e2d10
Héctor Miguel <hmga312@hotmail.com>
2025-11-12 20:00:28 -0600

    Configuro plantillas fuera de la app blog y corrigo rutas de vista

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -66,7 +66,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
