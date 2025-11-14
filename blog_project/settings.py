1d64e2e54f09b8feb86ed1166a27200e6c60b584
Héctor Miguel <hmga312@hotmail.com>
2025-11-13 20:18:42 -0600

    seguimooos hartos

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -50,12 +50,11 @@ INSTALLED_APPS = [
     'cloudinary_storage',
 ]
 
-cloudinary.config( 
-  cloud_name = config('CLOUDINARY_CLOUD_NAME'),
-  api_key = config('CLOUDINARY_API_KEY'),
-  api_secret = config('CLOUDINARY_API_SECRET'),
-  secure = True
-)
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
